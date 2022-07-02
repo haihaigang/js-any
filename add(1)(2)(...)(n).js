cc59584de05b2e26df5a857c305f5777963657b0
“wanghaigang” <haihaigang@163.com>
2022-07-02 12:17:01 +0800

    fix: modify add any

--- add(1)(2)(...)(n).js
+++ add(1)(2)(...)(n).js
@@ -1,36 +1,26 @@
 // add(1)(3)(6)(9) => 19
 
-function create(f) {
-    let queue = []
-    let going = false
-    return function(n){
-        queue.push(n)
-
-        if (!going) {
-            going = true
-
-            while(queue.length) {
-                value = f.call(null, queue.shift())
-            }
-            going = false
-            return total
-        }
-    }
-}
-
-function bountadd() {
-    // let args = [...arguments].reduce((a, b) => a+b)
-    // let temp =  bountadd.bind(null, args)
-    // temp.toString = function() {}
-    // return temp
-    let sum = [...arguments].reduce((a, b) => a+b)
-    function ab(){
-        sum += [...arguments].reduce((a, b) => a+b)
-        return ab
-    }
-
-    ab.toString = function(){return sum}
-
+function add2() {
+  let sum = [...arguments].reduce((a, b) => a + b)
+  function ab() {
+    sum += [...arguments].reduce((a, b) => a + b)
     return ab
+  }
+
+  ab.toString = function () {
+    return sum
+  }
+
+  return ab
 }
-bountadd(1)(2)(3)
+;+add2(1, 2)(3)(4, 5)
+
+function add() {
+  let args = [...arguments]
+  let temp = add.bind(null, ...args)
+  temp.toString = function () {
+    return args.reduce((a, b) => a + b)
+  }
+  return temp
+}
+;+add(1, 2)(3)(4, 5)
